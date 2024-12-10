835cab5e1e6b139f20a4268b580cd2f7ca1977d8
CHAPELLIERE Youenn <y.chapelliere@rns.ecolescreatives.com>
2024-12-10 10:08:29 +0100

    [ Update ] Update UI

--- Assets/SCRIPTS/BookMark/Bookmark_Open.cs
+++ Assets/SCRIPTS/BookMark/Bookmark_Open.cs
@@ -8,7 +8,6 @@ public class Bookmark_Open : MonoBehaviour
     [SerializeField] private GameObject _bookmarkPanel1;
     [SerializeField] private GameObject _bookmarkPanel2;
     [SerializeField] private GameObject _bookmarkPanel3;
-    [SerializeField] private GameObject _bookmarkPanel4;
 
     [Header("Sound Effect"), Space(5)]
     [SerializeField] private AudioClip _soundEffect1;
@@ -20,7 +19,6 @@ public class Bookmark_Open : MonoBehaviour
         _bookmarkPanel1.SetActive(true);
         _bookmarkPanel2.SetActive(false);
         _bookmarkPanel3.SetActive(false);
-        _bookmarkPanel4.SetActive(false);
 
         int randomSoundEffect = Random.Range(1, 3);
         
@@ -39,7 +37,6 @@ public class Bookmark_Open : MonoBehaviour
         _bookmarkPanel1.SetActive(false);
         _bookmarkPanel2.SetActive(true);
         _bookmarkPanel3.SetActive(false);
-        _bookmarkPanel4.SetActive(false);
 
         int randomSoundEffect = Random.Range(1, 3);
 
@@ -58,26 +55,6 @@ public class Bookmark_Open : MonoBehaviour
         _bookmarkPanel1.SetActive(false);
         _bookmarkPanel2.SetActive(false);
         _bookmarkPanel3.SetActive(true);
-        _bookmarkPanel4.SetActive(false);
-
-        int randomSoundEffect = Random.Range(1, 3);
-
-        if (randomSoundEffect == 1)
-        {
-            _audioSource.PlayOneShot(_soundEffect1);
-        }
-        else
-        {
-            _audioSource.PlayOneShot(_soundEffect2);
-        }
-    }
-    
-    public void OpenBookmark4()
-    {
-        _bookmarkPanel1.SetActive(false);
-        _bookmarkPanel2.SetActive(false);
-        _bookmarkPanel3.SetActive(false);
-        _bookmarkPanel4.SetActive(true);
 
         int randomSoundEffect = Random.Range(1, 3);
 
