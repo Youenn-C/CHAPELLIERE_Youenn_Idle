4560c39d1a4e6b59771fd52f5ccade0779fcc3d7
CHAPELLIERE Youenn <y.chapelliere@rns.ecolescreatives.com>
2024-12-17 09:52:42 +0100

    [ Update ] activation du lien pour l'amélioration du tavernier

--- Assets/SCRIPTS/Tavernier.cs
+++ Assets/SCRIPTS/Tavernier.cs
@@ -7,17 +7,21 @@ using TMPro;
 
 public class Tavernier : MonoBehaviour
 {
+    [Header("Reference"), Space(5)]
     [SerializeField] private GameObject _empoyTextButton;
     [SerializeField] private GameObject _levelUpButton;
     [SerializeField] private Button _tavernierButton;
     [SerializeField] private int tavernierLevel;
     [SerializeField] private int multiplyingFactor = 1;
+    [SerializeField] private TMP_Text _levelText;
+    
     // Start is called before the first frame update
     void Start()
     {
         _empoyTextButton.SetActive(true);
         _levelUpButton.SetActive(false);
         tavernierLevel = 0;
+        _levelText.text = "Inactif";
     }
 
     public void UpgradeTavernier()
@@ -61,5 +65,6 @@ public class Tavernier : MonoBehaviour
         _tavernierButton.interactable = false;
         yield return new WaitForSeconds(0.25f);
         _tavernierButton.interactable = true;
+        _levelText.text = "Lvl : " + tavernierLevel.ToString();
     }
 }
